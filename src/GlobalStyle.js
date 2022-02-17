d86edc9fad97467f1067b29092d3bbaded8a9f1a
Julián David Sánchez Gallego <50778235+juliansg022@users.noreply.github.com>
2022-02-17 17:27:06 -0500

    Add missing global style

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -9,6 +9,7 @@ const GlobalStyle = createGlobalStyle`
         --darkGrey: #1c1c1c;
         --fontSuperBig: 2.5rem;
         --fontBig: 1.5rem;
+        --fondMed: 1.2rem;
         --fontSmall: 1rem;
     }
 
