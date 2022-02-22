9f5f76de1211ffb294feb40dd50fbfe6ee77b749
Julián David Sánchez Gallego <50778235+juliansg022@users.noreply.github.com>
2022-02-22 00:00:33 -0500

    Implement functions to load additional movies and movies by search field. Implement function to save the session storage

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -3,6 +3,9 @@ import { useState, useEffect } from 'react';
 // API
 import API from '../API';
 
+// Helpers
+import { isPersistedState } from '../helpers';
+
 const initialState = {
   page: 0,
   results: [],
@@ -11,21 +14,23 @@ const initialState = {
 };
 
 const useHomeFetch = () => {
+  const [searchTerm, setSearchTerm] = useState('');
   const [state, setState] = useState(initialState);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [isLoadingMore, setIsLoadingMore] = useState(false);
 
-  const fetchMovies = async (page, searchTerm = '') => {
+  const fetchMovies = async (page, theSearchTerm = '') => {
     try {
       setError(false);
       setLoading(true);
 
-      const movies = await API.fetchMovies(searchTerm, page);
+      const movies = await API.fetchMovies(theSearchTerm, page);
 
       setState((prev) => ({
         ...movies,
         results:
-            page > 1 ? [...prev.results, ...movies.results] : [...movies.results],
+          page > 1 ? [...prev.results, ...movies.results] : [...movies.results],
       }));
     } catch (e) {
       setError(true);
@@ -33,12 +38,38 @@ const useHomeFetch = () => {
     setLoading(false);
   };
 
-  // Initial render
+  // Initial and search
   useEffect(() => {
-    fetchMovies(1);
-  }, []);
+    if (!searchTerm) {
+      const sessionState = isPersistedState('homeState');
 
-  return { state, loading, error };
+      if (sessionState) {
+        // console.log('Grabbing from sessionStorage');
+        setState(sessionState);
+        return;
+      }
+    }
+    // console.log('Grabbing from API');
+    setState(initialState);
+    fetchMovies(1, searchTerm.trim().length ? searchTerm : undefined);
+  }, [searchTerm]);
+
+  // Load more
+  useEffect(() => {
+    if (!isLoadingMore) return;
+
+    fetchMovies(state.page + 1, searchTerm);
+    setIsLoadingMore(false);
+  }, [isLoadingMore, searchTerm, state.page]);
+
+  // Write to sessionStorage
+  useEffect(() => {
+    if (!searchTerm) sessionStorage.setItem('homeState', JSON.stringify(state));
+  }, [searchTerm, state]);
+
+  return {
+    state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore,
+  };
 };
 
 export default useHomeFetch;
