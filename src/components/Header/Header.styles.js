27bef8f78bf646e41202a8027b302c747d9ecdb4
Julián David Sánchez Gallego <50778235+juliansg022@users.noreply.github.com>
2022-02-21 23:46:15 -0500

    Complete the Header component and its styles

--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -1,17 +1,17 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-    background: var(--darkGrey);
-    padding: 0 20px;
+  background: var(--darkGrey);
+  padding: 0 20px;
 `;
 
 export const Content = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    max-width: var(--maxWidth);
-    padding: 20px 0;
-    margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: var(--maxWidth);
+  padding: 20px 0;
+  margin: 0 auto;
 `;
 
 export const LogoImg = styled.img`
@@ -24,7 +24,7 @@ export const LogoImg = styled.img`
 
 export const TMDBLogoImg = styled.img`
   width: 100px;
-  
+
   @media screen and (max-width: 500px) {
     width: 80px;
   }
